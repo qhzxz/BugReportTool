913ba6e99b4e6f4647301a8dcce423499e302e15
hqin@telenavsoftware.com <hqin@telenavsoftware.com>
2025-10-13 18:12:09 +0800

    init commit

--- lib/database/app_database.dart
+++ lib/database/app_database.dart
@@ -1,4 +1,5 @@
 import 'package:drift/drift.dart';
+import 'package:drift/web.dart';
 import 'package:drift/native.dart';
 import 'dart:io';
 import '../model/status.dart';
@@ -6,19 +7,22 @@ import 'entity/ticket_entity.dart';
 
 part 'app_database.g.dart';
 
-
 @DriftDatabase(tables: [TicketEntity])
 class AppDatabase extends _$AppDatabase {
-  AppDatabase(File dbDir) :super(_openConnection(dbDir));
+  AppDatabase(File dbDir) : super(_openConnection(dbDir));
 
   @override
   int get schemaVersion => 1;
-
 }
 
 LazyDatabase _openConnection(File dbDir) {
-  return LazyDatabase(() async {
-
-    return NativeDatabase.createInBackground(dbDir);
-  });
+  if (Platform.isMacOS || Platform.isWindows) {
+    return LazyDatabase(() async {
+      return NativeDatabase.createInBackground(dbDir);
+    });
+  } else {
+    return LazyDatabase(() async {
+      return WebDatabase('my_db'); // Web 自动用 IndexedDB
+    });
+  }
 }
